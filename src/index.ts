ab73422fbf2a473b3e18912e9679be4b50d13b12
hdh3296 <hdh3296@gmail.com>
2022-12-17 13:51:06 +0900

    blue

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'han dong hoon grey';
+  const name = 'han dong hoon blue';
   greetUser(name);
-  document.body.style.backgroundColor = 'grey';
+  document.body.style.backgroundColor = 'blue';
 });
