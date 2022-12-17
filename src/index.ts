fb6f56e94ffbcf5cb3b969a49e3dc415dd9c7e0f
hdh3296 <hdh3296@gmail.com>
2022-12-17 12:07:47 +0900

    test1
    배경색 : 레드

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
+  const name = 'donghoon han';
   greetUser(name);
+  document.body.style.backgroundColor = 'red';
 });
