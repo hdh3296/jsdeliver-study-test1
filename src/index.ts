21a5e4e77eb981724785167fbdc5fc922ba41cf3
hdh3296 <hdh3296@gmail.com>
2022-12-17 12:41:56 +0900

    name 변경함

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'han dong hoon';
+  const name = 'han dong hoon2';
   greetUser(name);
   document.body.style.backgroundColor = 'black';
 });
